832217c509eb3fdc71c409205303e17cd6a19fed
marusosasssi <soassi@outlook.com>
2022-11-15 23:25:16 -0600

    import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO clients (id, name, lastname, email, created_at) VALUES (1, 'Maru', 'Sosa', 'maru@marusosa.com', '2022-11-14');
+INSERT INTO clients (id, name, lastname, email, created_at) VALUES (2, 'Rafael', 'Flores', 'rafa@marusosa.com', '2022-11-15');
+INSERT INTO clients (id, name, lastname, email, created_at) VALUES (3, 'Nayeon', 'Im', 'nayeon@marusosa.com', '2022-11-16');
+INSERT INTO clients (id, name, lastname, email, created_at) VALUES (4, 'Jeongyeon', 'Yoo', 'jeong@marusosa.com', '2022-11-17');
+INSERT INTO clients (id, name, lastname, email, created_at) VALUES (5, 'Momo', 'Hirai', 'momo@marusosa.com', '2022-11-18');
+INSERT INTO clients (id, name, lastname, email, created_at) VALUES (6, 'Sana', 'Minatozaki', 'sana@marusosa.com', '2022-11-19');
+INSERT INTO clients (id, name, lastname, email, created_at) VALUES (7, 'Jihyo', 'Park', 'jihyo@marusosa.com', '2022-11-20');
